356d1f4559b76d6917c488b280fe33237994ca78
Piyush Agrawal <piyushagrawal886@gmail.com>
2024-07-16 06:29:34 +0530

    error handling

--- first-app/src/app/product/[productid]/review/[reviewid]/page.tsx
+++ first-app/src/app/product/[productid]/review/[reviewid]/page.tsx
@@ -1,10 +1,22 @@
+"use client";
+import { error } from "console";
 import {notFound} from "next/navigation";
+
+function getRandomInt(count: number){
+    return Math.floor(Math.random()*count);
+}
+
 export default function ReviewID({params}:{
     params:{
         productid:string,
         reviewid:string,
     }}
 ){
+    const random=getRandomInt(2);
+
+    if(random==1)
+        throw new Error("Error loading review");
+    
     if(parseInt(params.reviewid)>1000)
         notFound();
 
